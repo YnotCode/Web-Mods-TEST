42ee5dfb099dec7736aad24719d4f250ba26d3e0
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-27 13:29:18 -0400

    Update prototype.js

--- gDark/prototype.js
+++ gDark/prototype.js
@@ -1,7 +1,7 @@
 function injectCode(){
 
  document.getElementById("hplogo").src = "https://lh3.googleusercontent.com/proxy/KJKiKv_TIyggQQKcEiIDnLbsdRwJG7SHBZSz59NphafKLH8VLe9QmRpis0EZxqoKpB1jCCSv6Jo74IbF9QxhNfEkziEKJCQd";
-
+ document.body.style.backgroundColor = "blue";
 }
 
 injectCode();
