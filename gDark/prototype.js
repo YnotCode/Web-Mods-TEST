44653ff66f6ca5bb6a4dd3f149df747b4890b301
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-27 13:33:49 -0400

    Update prototype.js

--- gDark/prototype.js
+++ gDark/prototype.js
@@ -1,8 +1,9 @@
 var timer;
+var url = "https://lh3.googleusercontent.com/proxy/KJKiKv_TIyggQQKcEiIDnLbsdRwJG7SHBZSz59NphafKLH8VLe9QmRpis0EZxqoKpB1jCCSv6Jo74IbF9QxhNfEkziEKJCQd";
 
 function injectCode(){
- if (document.body.style.backgroundColor != "blue"){
-  document.getElementById("hplogo").src = "https://lh3.googleusercontent.com/proxy/KJKiKv_TIyggQQKcEiIDnLbsdRwJG7SHBZSz59NphafKLH8VLe9QmRpis0EZxqoKpB1jCCSv6Jo74IbF9QxhNfEkziEKJCQd";
+ if (document.body.style.backgroundColor != "blue" || document.getElementById("hplogo").src != url){
+  document.getElementById("hplogo").src = url;
   document.body.style.backgroundColor = "blue";
  }
  else{
