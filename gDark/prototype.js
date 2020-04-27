98806af854a0a1dcb6f335a89298d3e4169d6e69
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-27 13:13:30 -0400

    Update prototype.js

--- gDark/prototype.js
+++ gDark/prototype.js
@@ -1,5 +1,7 @@
 window.onload = function(){
 
- var a = "B";
+ var img = document.createElement("img");
+ img.src = "https://lh3.googleusercontent.com/proxy/KJKiKv_TIyggQQKcEiIDnLbsdRwJG7SHBZSz59NphafKLH8VLe9QmRpis0EZxqoKpB1jCCSv6Jo74IbF9QxhNfEkziEKJCQd";
+ document.appendChild(img);
 
 }
