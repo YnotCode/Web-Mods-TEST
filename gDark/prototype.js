fb1393f7e940ef4c1ecfaeeac025238778f6e477
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-27 13:25:22 -0400

    Update prototype.js

--- gDark/prototype.js
+++ gDark/prototype.js
@@ -1,13 +1,6 @@
 function injectCode(){
 
- var img = document.createElement("img");
- img.src = "https://lh3.googleusercontent.com/proxy/KJKiKv_TIyggQQKcEiIDnLbsdRwJG7SHBZSz59NphafKLH8VLe9QmRpis0EZxqoKpB1jCCSv6Jo74IbF9QxhNfEkziEKJCQd";
- img.style.position = absolute;
- img.style.top = 10;
- img.style.left = 10;
- img.style.width = "200px";
- img.style.height = "200px";
- document.body.appendChild(img);
+ document.body.style.backgroundColor = "white";
 
 }
 
