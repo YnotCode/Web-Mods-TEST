8bc07dcb660e285210ff924b513d1f162083028c
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-27 13:36:45 -0400

    Update prototype.js

--- gDark/prototype.js
+++ gDark/prototype.js
@@ -2,14 +2,12 @@ var timer;
 var url = "https://lh3.googleusercontent.com/proxy/KJKiKv_TIyggQQKcEiIDnLbsdRwJG7SHBZSz59NphafKLH8VLe9QmRpis0EZxqoKpB1jCCSv6Jo74IbF9QxhNfEkziEKJCQd";
 
 function injectCode(){
- if (document.body.style.backgroundColor != "blue" || document.getElementById("hplogo").src != url){
-  document.getElementById("hplogo").src = url;
+ if (document.body.style.backgroundColor != "blue"){
+  document.getElementById("hplogo").childNodes[0].childNodes[0].src = url;
   document.body.style.backgroundColor = "blue";
  }
  else{
   clearInterval(timer);
  }
 }
-
-
 timer = window.setInterval(injectCode, 1000);
