65e1fbd2af2ece0934c7cdba83caa4291eb5365b
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-28 17:08:14 -0400

    Update prototype.js

--- googleDoodle/prototype.js
+++ googleDoodle/prototype.js
@@ -1,1 +1,22 @@
+var looper;
+var doneInjecting = false;
 
+
+function injectCode(){
+  
+ if (!doneInjecting){
+  
+   var logo = document.getElementById("hplogo").childNodes[0].childNodes[0].src;
+   console.log("SOURCE:" + logo);
+   doneInjecting = true;
+   
+ }
+ else{
+    clearInterval(looper);
+ }
+  
+  
+  
+}
+
+looper = setInterval(injectCode, 1000);
