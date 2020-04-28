4abc40d8d50156b57d0cbea55fb2f81aeecf6171
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-28 17:35:20 -0400

    Update prototype.js

--- googleDoodle/prototype.js
+++ googleDoodle/prototype.js
@@ -1,6 +1,28 @@
 var url = "https://github.com/YnotCode/Web-Mods-TEST/blob/master/googleDoodle/googleDoodle.png?raw=true";
 var images = document.getElementsByTagName("img");
-
+var links = document.getElementsByTagName("a");
+for (var a = 0; a < links.length; a++){
+  
+  links[a].href = "https://www.lexaloffle.com/bbs/?tid=2145";
+  
+}
 images[0].src = url;
 document.body.style.backgroundColor = "black";
 console.log("V.1.1");
+
+var newFlakes = [];
+var movingFlakes = [];
+
+function snowfall(){
+   
+   for (var i = 0; i < 10; i++){
+     var newSnowflake = document.createElement("div");
+     newSnowflake.class = "snowflake";
+     document.body.appendChild(newSnowflake);
+   }
+   
+}
+
+
+snowfall();
+//setInterval(snowfall, 17)
