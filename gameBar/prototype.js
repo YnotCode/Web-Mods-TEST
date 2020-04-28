19fee832d886dc9c91f13298042376c3adebcd66
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-27 23:04:00 -0400

    Create prototype.js

---
+++ gameBar/prototype.js
@@ -0,0 +1,22 @@
+var mainExec;
+var block;
+
+function mainInjection(){
+
+  if (block.id == undefined){
+    block = document.createElement("div");
+    block.id = "mainBox";
+    block.style.backgroundColor = "red";
+    block.style.position = "absolute";
+    block.style.top = "10px";
+    block.style.left = "10px";
+    document.body.appendChild(block);
+  }
+  else{
+    window.clearInterval(mainExec);
+  }
+
+
+}
+
+mainExec = window.setInterval(mainInjection, 1000);
