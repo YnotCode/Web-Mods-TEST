e9f3943295a187dd179ee5665b89e75f45c64135
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-29 11:58:41 -0400

    Update prototype.js

--- googleDoodle/prototype.js
+++ googleDoodle/prototype.js
@@ -1,11 +1,6 @@
 var url = "https://github.com/YnotCode/Web-Mods-TEST/blob/master/googleDoodle/googleDoodle.png?raw=true";
 var images = document.getElementsByTagName("img");
-var links = document.getElementsByTagName("a");
-for (var a = 0; a < links.length; a++){
-  
-  links[a].href = "https://www.lexaloffle.com/bbs/?tid=2145";
-  
-}
+
 images[0].src = url;
 document.body.style.backgroundColor = "black";
 console.log("V.1.1");
